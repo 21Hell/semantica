f5854d55d9678ebb8f22faa261377258050affad
Melendez Ivan <l20140919@queretaro.tecnm.mx>
2022-09-30 10:19:29 -0500

    Empezamos aValidar FOr aun no funca

--- prueba.cpp
+++ prueba.cpp
@@ -6,12 +6,19 @@
 #include <conio.h>
 float resultado;
 float area, radio, pi;
-char x;
 int d;
+float x;
+char y;   
 // Este programa calcula el volumen de un cilindro.
 void main()
 {
+    y = x;
     x = (float)(1);
+    for (i=1;i<10;i++){
+        printf("\nhola");
+    }
+
+
     if(3+5>10)
     {
         if(1==1)

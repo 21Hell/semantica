02fb5a74988cfbc86d9342a577f96f55c093388e
Cwolf <l20140919@queretaro.tenm.mx>
2022-10-08 18:02:37 -0500

    reconciliando ramas

--- prueba.cpp
+++ prueba.cpp
@@ -15,18 +15,10 @@ int h;
 // Este programa calcula el volumen de un cilindro.
 void main()
 {
-    j = 1;
-    for (j=1;j<10;j++){
-        for (h = 0; h < 3; h++)
-        {
-            printf("hola\n");
-            printf(j);
-            printf("\n");
-            printf(h);
-            printf("\n");
-        }
-        
-    }
+
+
+    y = (char)(256);
+    printf(y);
 
 
 
